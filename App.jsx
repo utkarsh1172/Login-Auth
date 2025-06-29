64fc85832f7b242f2fd62264a61ef315784aaf60
Utkarsh Kamble <utkarsh.ipaypro@gmail.com>
2025-06-29 12:04:12 +0530

    Latest commit

--- App.jsx
+++ App.jsx
@@ -2,6 +2,7 @@ import {createStackNavigator} from '@react-navigation/stack';
 import {View, Text, Button} from 'react-native';
 import {NavigationContainer, useNavigation,DrawerActions} from '@react-navigation/native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import HomeScreen from './Screens/HomeScreen';
 import ProfileScreen from './Screens/ProfileScreen';
 import UserScreen from './Screens/UserScreen';
@@ -9,37 +10,117 @@ import 'react-native-gesture-handler'
 import Icon  from 'react-native-vector-icons/Entypo';
 import DrawerContent from './DrawerContent';
 import SplashScreen from 'react-native-splash-screen';
-import { useEffect } from 'react';
+import { useEffect,useState } from 'react';
 import LoginPage from './Screens/Login&Register/Login';
 import RegisterPage from './Screens/Login&Register/Register';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import Toast, { BaseToast, ErrorToast } from 'react-native-toast-message';
+import UpdateProfile from './Screens/UpdateProfile/UpdateProfile';
 
 
+const toastConfig = {
+ 
+  success: (props) => (
+    <BaseToast
+      {...props}
+      style={{
+        borderLeftColor: 'green',
+        borderLeftWidth: 7,
+        width: '90%',
+        height: 70,
+        borderRightColor: 'green',
+        borderRightWidth: 7,
+      }}
+      contentContainerStyle={{paddingHorizontal: 15}}
+      text1Style={{
+        fontSize: 17,
+        fontWeight: '700',
+      }}
+      text2Style={{
+        fontSize: 14,
+      }}
+    />
+  ),
+  /*
+    Overwrite 'error' type,
+    by modifying the existing `ErrorToast` component
+  */
+  error: (props) => (
+    <ErrorToast
+      {...props}
+      text2NumberOfLines={3}
+      style={{
+        borderLeftColor: 'red',
+        borderLeftWidth: 7,
+        width: '90%',
+        height: 70,
+        borderRightColor: 'red',
+        borderRightWidth: 7,
+      }}
+      contentContainerStyle={{paddingHorizontal: 15}}
+      text1Style={{
+        fontSize: 17,
+        fontWeight: '700',
+      }}
+      text2Style={{
+        fontSize: 14,
+      }}
+    />
+  ),
+  
+};
 const StackNav = () => {
-  const Stack = createStackNavigator();
-  const navigation = useNavigation()
+  const Stack = createNativeStackNavigator();
+  const navigation = useNavigation();
   return (
     <Stack.Navigator
-    screenOptions={{
-      statusBarColor: '#0163d2',
-      headerStyle: {backgroundColor: '#0163d2'},
-      headerTintColor: '#fff',
-      headerTitleAlign: 'center',
-      headerLeft: () => {
-        return(
-          <Icon
-          onPress={() => navigation.dispatch(DrawerActions.openDrawer())}
-          name="menu"
-          size={30}
-          color='#fff' />
-        )
-      }
-    }}>
-    <Stack.Screen name='Home' component={HomeScreen}/>
-    <Stack.Screen name='Profile' component={ProfileScreen}/>
-    <Stack.Screen name='User' component={UserScreen} options={{headerShown:true}}/>
-  </Stack.Navigator>
-  )
-}
+      screenOptions={{
+        statusBarColor: '#0163d2',
+        headerShown: false,
+        headerStyle: {
+          backgroundColor: '#0163d2',
+        },
+        headerTintColor: '#fff',
+        headerTitleAlign: 'center',
+      }}>
+      <Stack.Screen
+        name="Home"
+        component={HomeScreen}
+        options={
+          {
+            // headerLeft: () => {
+            //   return (
+            //     <Icon
+            //       name="menu"
+            //       onPress={() => navigation.dispatch(DrawerActions.openDrawer())}
+            //       size={30}
+            //       color="#fff"
+            //     />
+            //   );
+            // },
+          }
+        }
+      />
+      <Stack.Screen name="Profile" component={ProfileScreen} />
+      <Stack.Screen
+        name="User"
+        component={UserScreen}
+        options={{
+          headerShown: true,
+        }}
+      />
+       <Stack.Screen
+        name="UpdateProfile"
+        component={UpdateProfile}
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Stack.Screen name="LoginUser" component={LoginNav}/>
+    
+    </Stack.Navigator>
+  );
+};
 
 const DrawerNav = () => {
   const Drawer= createDrawerNavigator()
@@ -54,23 +135,38 @@ const DrawerNav = () => {
   </Drawer.Navigator>
   )
 }
+const LoginNav = () => {
+  const Stack = createNativeStackNavigator();
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        headerShown: false,
+      }}>
+      <Stack.Screen name="Login" component={LoginPage} />
+      <Stack.Screen name="Register" component={RegisterPage} />
+      <Stack.Screen name="Home" component={DrawerNav} />
+    </Stack.Navigator>
+  );
+};
 function App() {
+const [isLoggedIn, setIsLoggedIn] = useState(false);
+  async function getData() {
+    const data = await AsyncStorage.getItem('isLoggedIn');
+    console.log(data, 'at app.jsx');
+    setIsLoggedIn(data);
+  }
 
   useEffect(() => {
-    setTimeout(() =>{
-      SplashScreen.hide()
-    },500)
+    getData();
+    setTimeout(() => {
+      SplashScreen.hide();
+    }, 900);
+  }, [isLoggedIn]);
 
-  }, [])
-    const Stack = createStackNavigator()
   return (
     <NavigationContainer>
-      <Stack.Navigator screenOptions={{
-        headerShown:false
-      }}>
-        <Stack.Screen name='Login' component={LoginPage}/>
-        <Stack.Screen name='Register' component={RegisterPage}/>
-      </Stack.Navigator>
+      {isLoggedIn ? <DrawerNav /> : <LoginNav />}
+      <Toast config={toastConfig}/>
     </NavigationContainer>
   );
 }
